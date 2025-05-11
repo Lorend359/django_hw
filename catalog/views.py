58272f17fe813f3d3802891bbc42afb4535b96fd
Svyatoslav <arend359@gmail.com>
2025-05-11 22:44:17 +0300

    made additional part

--- catalog/views.py
+++ catalog/views.py
@@ -3,9 +3,13 @@ from .models import Product, Contact
 
 def home(request):
     latest = Product.objects.order_by("-created_at")[:5]
-    print(">>> Latest 5 products:", list(latest))   # выводим в консоль
-    return render(request, "catalog/home.html", {"latest_products": latest})
+    print(">>> Latest 5 products:", list(latest))
+    return render(request, "catalog/home.html", {
+        "latest_products": latest,
+    })
 
 def contacts(request):
     contacts = Contact.objects.all()
-    return render(request, "catalog/contacts.html", {"contacts": contacts})
+    return render(request, "catalog/contacts.html", {
+        "contacts": contacts,
+    })
